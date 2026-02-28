84dcdff63bca332abbc3ccb7994e5fc7ccfd720e
havario <157877551+havario@users.noreply.github.com>
2026-02-28 23:19:09 +0800

    style(ipinfo): improve comment clarity in is_in_china function
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- ipquery.sh
+++ ipquery.sh
@@ -98,14 +98,13 @@ curl() {
 
 is_in_china() {
     if [ -z "$COUNTRY" ]; then
-        # www.cloudflare.com / dash.cloudflare.com 国内访问的是美国服务器，而且部分地区被墙
+        # www.cloudflare.com / dash.cloudflare.com 国内访问的是美国服务器 而且部分地区被墙
         # 没有ipv6 www.visa.cn
         # 没有ipv6 www.bose.cn
         # 没有ipv6 www.garmin.com.cn
         # 备用 www.prologis.cn
         # 备用 www.autodesk.com.cn
         # 备用 www.keysight.com.cn
-
         if ! COUNTRY="$(curl -Ls http://www.qualcomm.cn/cdn-cgi/trace | grep '^loc=' | cut -d= -f2 | grep .)"; then
             die "Can not get location."
         fi
